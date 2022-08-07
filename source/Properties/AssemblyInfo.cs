240537be805ca35adc821bd7283e2e749e800c33
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2022-08-06 22:01:18 -0500

    Update AssemblyInfo.cs

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -13,24 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Sarbian 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec9f45e9-2c5f-4503-9d1c-7e5cbe128f12")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: Guid("EC9F45E9-2C5F-4503-9D1C-7E5CBE128F12")]
